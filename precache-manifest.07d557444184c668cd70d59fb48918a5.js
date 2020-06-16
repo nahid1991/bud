3c52934fad4020826e10e626484fadd44cfdf889
Nahid <nahidshaiket10300@gmail.com>
2020-06-16 22:10:06 +0600

    Updates

---
+++ precache-manifest.07d557444184c668cd70d59fb48918a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34d24692a462419c0d72e8ae32de835",
+    "url": "/bud/index.html"
+  },
+  {
+    "revision": "0330888b465d67bc4101",
+    "url": "/bud/static/css/main.89361ecf.chunk.css"
+  },
+  {
+    "revision": "712486c5b9eb564bb003",
+    "url": "/bud/static/js/2.fbddd8f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bud/static/js/2.fbddd8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0330888b465d67bc4101",
+    "url": "/bud/static/js/main.b23d07b7.chunk.js"
+  },
+  {
+    "revision": "33e24c3e29c84e02bd62",
+    "url": "/bud/static/js/runtime-main.f01267e4.js"
+  }
+]);
