27186559fc3c43eb4bed41fcd5a965f2fe04d460
Nahid <nahidshaiket10300@gmail.com>
2020-06-24 00:40:17 +0600

    testing pdf download

--- src/components/General/GeneralCV.js
+++ src/components/General/GeneralCV.js
@@ -1,19 +1,51 @@
 import React, {Component} from 'react';
 import Banner from "./Banner";
-import {Link} from 'react-router-dom';
+import {connect} from "react-redux";
+import axios from 'axios';
+
+// import {Link} from 'react-router-dom';
 
 class GeneralCv extends Component {
+  handleDownload = () => {
+    axios.post('https://secret-castle-60004.herokuapp.com/api/v1/process-pdf',
+      {name: this.props.name, avatar: this.props.avatar})
+      .then(function (response) {
+        const linkSource = `data:application/pdf;base64,${response.data.pdf}`;
+        const downloadLink = document.createElement("a");
+        const fileName = "cv.pdf";
+
+        downloadLink.href = linkSource;
+        downloadLink.download = fileName;
+        downloadLink.click();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <div className="root">
         <Banner/>
-        <div style={{width: "100%", flexDirection: "column", display: "flex",
-          justifyContent: "flex-end", alignItems: "center", marginRight: "0px"}}>
-          <Link className="btn-primary" to={"/bud/test"}>DOWNLOAD AS PDF</Link>
+        <div style={{
+          width: "100%", flexDirection: "column", display: "flex",
+          justifyContent: "flex-end", alignItems: "center", marginRight: "0px"
+        }}>
+          <button className="btn-primary" onClick={() => {
+            this.handleDownload()
+          }}>DOWNLOAD AS PDF
+          </button>
         </div>
       </div>
     );
   }
 }
 
-export default GeneralCv;
+const mapStateToProps = state => {
+  return {
+    avatar: state.rootreducer.avatar,
+    name: state.rootreducer.name
+  };
+};
+
+export default connect(mapStateToProps)(GeneralCv);
