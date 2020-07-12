e35eb00acd8d4d5722e9d769c9c2e33dcac8d62f
Nahid <nahidshaiket10300@gmail.com>
2020-07-12 21:33:50 +0200

    trying out media net

--- src/components/General/Topbar.js
+++ src/components/General/Topbar.js
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 class Topbar extends Component {
   render() {
     return (
-      <div style={{width: "100%", background: "#546e7a", top: "0px", position: "absolute", textAlign: "left",
-        paddingLeft: "90px", color: "#fff"}}>
+      <div style={{width: "93%", background: "#546e7a", top: "0px", position: "absolute", textAlign: "left",
+        paddingLeft: "100px", color: "#fff"}}>
         <div style={{cursor: "pointer"}} onClick={() => {window.location.href = "/"}}>
           <h2 style={{marginBlockEnd: "0px", marginBlockStart: "0px", float: "left", marginRight: "15px"}}>Resume Point</h2>
           <small style={{marginTop: "5px", float: "left"}}>Completely free and easy resume generator</small>
